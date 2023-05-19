c8de6d37d2c0531891ce98a9092c0dccf67a25b6
DuyHoang <duyhoang220904@gmail.com>
2023-05-19 22:49:43 +0700

    dashboard UI

--- src/view/dashboard/DashboardPage.jsx
+++ src/view/dashboard/DashboardPage.jsx
@@ -6,33 +6,38 @@ import {
   UserOutlined,
 } from "@ant-design/icons";
 import { Table } from "antd";
+import { BASE_URL } from "../../utils/apiConfig";
 import TitlePageAdmin from "../../common/title-page-admin/title-page-admin";
 import useGenres from "../../hook/useGenres";
-import useSeries from "../../hook/useSeries";
-import useMovie from "../../hook/useMovie";
+import useMedia from "../../hook/useMedia";
+import useUser from "../../hook/useUser";
+import { Link } from "react-router-dom";
 
 const DashboardPage = () => {
   const { genresList, getAllGenresRequest } = useGenres();
-  const { movieList, getAllMovieRequest } = useMovie();
-  const { seriesMovieList, getAllSeriesRequest } = useSeries();
+  const { mediaList, getMovieAndSeries } = useMedia();
+  const { userList, getAllUserRequest } = useUser();
 
   useEffect(() => {
     Promise.all([
-      getAllMovieRequest(),
       getAllGenresRequest(),
-      getAllSeriesRequest(),
+      getMovieAndSeries(),
+      getAllUserRequest(),
     ]);
   }, []);
+
+  const seriesList = mediaList.filter((item) => item.isSeries);
+  const moviesList = mediaList.filter((item) => !item.isSeries);
 
   const data = [
     {
       title: "Movie",
-      count: movieList?.length,
+      count: moviesList?.length,
       icon: <BarChartOutlined />,
     },
     {
       title: "Series",
-      count: seriesMovieList?.length,
+      count: seriesList?.length,
       icon: <BarChartOutlined />,
     },
     {
@@ -43,154 +48,104 @@ const DashboardPage = () => {
   ];
 
   const dataWidget = [
+    //media
     {
+      link: "/admin/movies",
       title: "Movie",
-      href: "/admin/movies",
-      listData: [
+      columns: [
         {
-          id: 1,
-          name: "	I Dream in Another Language",
-          category: "Movie",
-          rating: "1",
+          title: "Image",
+          key: "image",
+          dataIndex: "image",
+          width: 100,
+          render: (image) => (
+            <img src={`${BASE_URL}/${image}`} alt="" className="image_movie" />
+          ),
         },
         {
-          id: 1,
-          name: "	I Dream in Another Language",
-          category: "Movie",
-          rating: "1",
+          title: "Name",
+          dataIndex: "name",
+          key: "name",
+          width: 10,
+          render: (name) => <span className="movie_name">{name}</span>,
         },
         {
-          id: 1,
-          name: "	I Dream in Another Language",
-          category: "Movie",
-          rating: "1",
+          title: "Category",
+          dataIndex: "isSeries",
+          key: "isSeries",
+          width: 10,
+          render: (isSeries) => (
+            <span className="category_movie">
+              {isSeries ? "Tv Show" : "Movie"}
+            </span>
+          ),
         },
         {
-          id: 1,
-          name: "	I Dream in Another Language",
-          category: "Movie",
-          rating: "1",
+          title: "Rating",
+          dataIndex: "rating",
+          key: "rating",
+          width: 10,
+          render: (rating) => (
+            <span className="rating_movie">
+              <span className="icon">&#x2605;</span>
+              {rating}
+            </span>
+          ),
         },
       ],
+      listData: mediaList,
     },
+
+    // user
     {
-      title: "Movie",
-      href: "/admin/movies",
-      listData: [
+      link: "/admin/users",
+      title: "User",
+      columns: [
         {
-          id: 1,
-          name: "	I Dream in Another Language",
-          category: "Movie",
-          rating: "1",
+          title: "Image",
+          key: "picturePath",
+          dataIndex: "picturePath",
+          width: 100,
+          render: (picturePath) => (
+            <img
+              src={
+                picturePath
+                  ? `${BASE_URL}/${picturePath}`
+                  : "https://png.pngitem.com/pimgs/s/649-6490124_katie-notopoulos-katienotopoulos-i-write-about-tech-round.png"
+              }
+              alt=""
+              className="image_user"
+            />
+          ),
         },
         {
-          id: 1,
-          name: "	I Dream in Another Language",
-          category: "Movie",
-          rating: "1",
+          title: "Username",
+          dataIndex: "username",
+          key: "username",
+
+          render: (username) => <span className="name_user">{username}</span>,
         },
         {
-          id: 1,
-          name: "	I Dream in Another Language",
-          category: "Movie",
-          rating: "1",
+          title: "Email",
+          dataIndex: "email",
+          key: "email",
+
+          render: (email) => <span className="email_user">{email}</span>,
         },
         {
-          id: 1,
-          name: "	I Dream in Another Language",
-          category: "Movie",
-          rating: "1",
+          title: "Role",
+          dataIndex: "isAdmin",
+          key: "isAdmin",
+
+          render: (isAdmin) => (
+            <span className="role_user">{isAdmin ? "ADMIN" : "USER"}</span>
+          ),
         },
       ],
-    },
-    {
-      title: "Movie",
-      href: "/admin/movies",
-      listData: [
-        {
-          id: 1,
-          name: "	I Dream in Another Language",
-          category: "Movie",
-          rating: "1",
-        },
-        {
-          id: 1,
-          name: "	I Dream in Another Language",
-          category: "Movie",
-          rating: "1",
-        },
-        {
-          id: 1,
-          name: "	I Dream in Another Language",
-          category: "Movie",
-          rating: "1",
-        },
-        {
-          id: 1,
-          name: "	I Dream in Another Language",
-          category: "Movie",
-          rating: "1",
-        },
-      ],
-    },
-    {
-      title: "Movie",
-      href: "/admin/movies",
-      listData: [
-        {
-          id: 1,
-          name: "	I Dream in Another Language",
-          category: "Movie",
-          rating: "1",
-        },
-        {
-          id: 1,
-          name: "	I Dream in Another Language",
-          category: "Movie",
-          rating: "1",
-        },
-        {
-          id: 1,
-          name: "	I Dream in Another Language",
-          category: "Movie",
-          rating: "1",
-        },
-        {
-          id: 1,
-          name: "	I Dream in Another Language",
-          category: "Movie",
-          rating: "1",
-        },
-      ],
+      listData: userList,
     },
   ];
 
-  const columns = [
-    {
-      title: "Id",
-      key: "id",
-      dataIndex: "id",
-      width: 10,
-    },
-    {
-      title: "Name",
-      key: "name",
-      dataIndex: "name",
-      width: 100,
-    },
-    {
-      title: "Category",
-      key: "category",
-      dataIndex: "category",
-      width: 100,
-    },
-    {
-      title: "Rating",
-      key: "rating",
-      dataIndex: "rating",
-      width: 100,
-    },
-  ];
   return (
     <div className="dashboard-container">
       <TitlePageAdmin title="DashBoard" />
@@ -211,10 +166,19 @@ const DashboardPage = () => {
             <div key={index} className="dashboard-widget-item">
               <div className="dashboard-widget-top">
                 <h1 className="dashboard-widget-title">{data.title}</h1>
-                <button className="dashboard-widget-view">View All</button>
+                <Link to={data.link}>
+                  <button className="dashboard-widget-view">View All</button>
+                </Link>
               </div>
               <div className="dashboard-widget-content">
-                <Table columns={columns} data={data.listData} />
+                <Table
+                  className="ant-table-content"
+                  columns={data.columns}
+                  dataSource={data.listData}
+                  scroll={{ x: "max-content", y: 400 }}
+                  rowKey={(record) => record._id}
+                  pagination={false}
+                />
               </div>
             </div>
           ))}
