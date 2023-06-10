70abbb05f98e831d8095069582c06767878bcf63
DuyHoang <duyhoang220904@gmail.com>
2023-06-10 16:31:02 +0700

    push code footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,43 @@
 import React from "react";
+import "./footer.scss";
+import {
+  YoutubeOutlined,
+  FacebookOutlined,
+  InstagramOutlined,
+  TwitterOutlined,
+} from "@ant-design/icons";
 
 const Footer = () => {
-  return <div className="footer">Footer</div>;
+  return (
+    <footer className="footer">
+      <div className="row">
+        <YoutubeOutlined />
+        <FacebookOutlined />
+        <InstagramOutlined />
+        <TwitterOutlined />
+      </div>
+      <div class="row">
+        <ul>
+          <li>
+            <a href="#">Contact us</a>
+          </li>
+          <li>
+            <a href="#">Our Services</a>
+          </li>
+          <li>
+            <a href="#">Privacy Policy</a>
+          </li>
+          <li>
+            <a href="#">Terms & Conditions</a>
+          </li>
+          <li>
+            <a href="#">Career</a>
+          </li>
+        </ul>
+      </div>
+      <div class="row">THANK YOU FOR WATCHING</div>
+    </footer>
+  );
 };
 
 export default Footer;
